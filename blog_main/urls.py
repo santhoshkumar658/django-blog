ab56ba02c125d4724ee19b9e9c781cdbc107b7e0
saravanakanishga777-source <saravanakanishga777@gmail.com>
2026-04-15 17:07:32 +0530

    blogs app and blog model

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import settings
+from django.conf.urls.static import static
 from . import views
 from django.contrib import admin
 from django.urls import path
@@ -21,4 +23,4 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ blogs/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Blog, Category
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('title',)}
+    list_display=('title','category','author','is_featured','status')
+    search_fields=('id','title','category__category_name','status')
+    list_editable = ('is_featured','status')
+admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
