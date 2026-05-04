c022a0de660337dba05329bfae7c7bc36ca2fe6f
saravanakanishga777-source <santhoshkumar18705@gmail.com>
2026-05-04 07:07:07 +0530

    comment feature

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 
-from . models import Blog, Category
+from . models import Blog, Category, Comment
 from django.db.models import Q
 
 # Create your views here.
@@ -26,16 +26,39 @@ def posts_by_category(request, category_id):
 
 def blogs(request, slug):
   single_blog = get_object_or_404(Blog, slug=slug, status='Published')
+  if request.method == 'POST':
+    comment = Comment()
+    comment.user = request.user
+    comment.blog = single_blog
+    comment.comment = request.POST['comment']
+    comment.save()
+    return redirect('blogs', slug=slug)
+
+  #comments 
+  comments = Comment.objects.filter(blog=single_blog)
+  comment_count = comments.count()
+ 
   context = {
     'single_blog': single_blog,
+    'comments': comments,
+    'comment_count': comment_count,
   }
   return render(request, 'blogs.html', context)
 
 def search(request):
-  keyword = request.GET.get('keyword')
-  blogs = Blog.objects.filter(Q(title__icontains=keyword)| Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword), status='Published')
+  keyword = (request.GET.get('keyword') or '').strip()
+  blogs = Blog.objects.filter(status='Published')
+  if keyword:
+    blogs = blogs.filter(
+      Q(title__icontains=keyword) |
+      Q(short_description__icontains=keyword) |
+      Q(blog_body__icontains=keyword)
+    )
+  else:
+    blogs = blogs.none()
   context= {
     'blogs': blogs,
     'keyword':keyword,
   }
   return render(request, 'search.html',context)
+
