6aeae1b8feb39705f5214ad37bd988e1225d7e26
saravanakanishga777-source <santhoshkumar18705@gmail.com>
2026-05-16 18:08:55 +0530

    Configure ALLOWED_HOSTS for deployment

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mpy79fd*g-4f-l-oaq_^^ig8hurtwj2(k@l=an&ciotev&q6v&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['saravanaadmin.pythonanywhere.com']
 
 
 # Application definition
