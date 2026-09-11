fcab8df26ed4e3d2507ae9f9301785cc94e0e80a
saravanakanishga777-source <santhoshkumar18705@gmail.com>
2026-09-11 16:59:35 +0530

    Prepare Django app for PythonAnywhere deployment

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-mpy79fd*g-4f-l-oaq_^^ig8hurtwj2(k@l=an&ciotev&q6v&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['saravanaadmin.pythonanywhere.com']
+ALLOWED_HOSTS = ['saravanaadmin.pythonanywhere.com','127.0.0.1' ]
 
 
 # Application definition
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'blog_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,4 +131,6 @@ STATICFILES_DIRS = [
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-CRISPY_TEMPLATE_PACK = 'Bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
